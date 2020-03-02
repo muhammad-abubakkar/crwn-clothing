b252efcea8185d545dd0157a3df5b11f7fc8c876
Abu Bakkar Siddique <muhammad.abubakkar@outlook.com>
2020-03-02 16:21:37 +0500

    Update our menu-items to navigate with withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import HomePage from "./pages/homepage/homepage";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route path="/" component={HomePage} />
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.js
+++ src/components/directory/directory.js
@@ -46,13 +46,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(section => (
-          <MenuItem
-            id={section.id}
-            title={section.title}
-            imageUrl={section.imageUrl}
-            size={section.size}
-          />
+        {this.state.sections.map(({ id, ...sectionProps }) => (
+          <MenuItem id={id} {...sectionProps} />
         ))}
       </div>
     );
